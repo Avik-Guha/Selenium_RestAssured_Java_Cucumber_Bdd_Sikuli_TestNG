b5a3b597439036280d81e3980147a9f843bfce95
Avik Guha <avikguha88@gmail.com>
2021-07-12 02:23:59 +0530

    updated runner file

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = { "src/test/resources/features" }, // feature folder path
 		glue = { "steps", "hooks" }, // step definition, hooks folder name
-		tags = { "@Regression" },
+		tags = { "@PlaceValidationE2E" },
 //		tags = {"@TestFile1 or @TestFile2 or @TestFile3"}, // to run multiple feature files with different tags
 		strict = true,
 		// dryRun = true,//to check feature vs step definition mapping is correct
